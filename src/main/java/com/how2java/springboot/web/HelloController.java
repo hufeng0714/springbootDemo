82a5c807663b0fdad2e0f79e17826d252505e424
Administrator <hufeng0714@163.com>
2018-12-12 13:46:15 +0800

    thymeleaf前后端分离

--- src/main/java/com/how2java/springboot/web/HelloController.java
+++ src/main/java/com/how2java/springboot/web/HelloController.java
@@ -16,13 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-  
+	  
     @RequestMapping("/hello")
-    public String hello(Model m) throws Exception {
-        m.addAttribute("now", DateFormat.getDateTimeInstance().format(new Date()));
-        if(true){
-            throw new Exception("some exception");
-        }
+    public String hello(Model m) {
+        m.addAttribute("name", "thymeleaf");
         return "hello";
     }
 }
